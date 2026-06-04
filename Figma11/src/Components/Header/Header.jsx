7051a8e24465684335a6088d09f12083e0416b9f
Mahliyo <gasandigital13@gmail.com>
2026-06-04 21:00:47 +0500

    Figma11 star header

--- Figma11/src/Components/Header/Header.jsx
+++ Figma11/src/Components/Header/Header.jsx
@@ -1,9 +1,12 @@
+import { useTranslation } from "react-i18next";
 import "./Header.css";
 import React from 'react'
+import i18next from "i18next";
 
 function Header() {
+  const [t, i18n] = useTranslation();
   return (
-    <div>Header</div>
+    <div></div>
   )
 }
 
