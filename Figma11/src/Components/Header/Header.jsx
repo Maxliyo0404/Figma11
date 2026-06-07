08fd59dc96de6777a6cd7791a134f5d36f7e84bd
Mahliyo <gasandigital13@gmail.com>
2026-06-07 23:56:41 +0500

    header strt css

--- Figma11/src/Components/Header/Header.jsx
+++ Figma11/src/Components/Header/Header.jsx
@@ -19,7 +19,6 @@ function Header() {
             <li className="header-item"><a href="#"></a></li>
             <li className="header-item"><a href="#"></a></li>
             <li className="header-item"><a href="#"></a></li>
-            <li className="header-item"><a href="#"></a></li>
           </ul>
         </div>
       </div>
