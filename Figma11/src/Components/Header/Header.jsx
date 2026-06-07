465e3945863d8f234203518a7f71450bd2f9f965
Mahliyo <gasandigital13@gmail.com>
2026-06-07 23:49:24 +0500

    header strt link

--- Figma11/src/Components/Header/Header.jsx
+++ Figma11/src/Components/Header/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
     <div className="header">
       <div className="container">
         <div className="header-wrapper">
-          <a href="#"><img src="" alt="" /></a>
+          <a className="" href="#"><img src="" alt="" /></a>
         </div>
       </div>
     </div>
